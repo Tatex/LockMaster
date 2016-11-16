94eb88527e613019f40f32feca389b035361329c
Antti Kyllönen <t4kyan01@students.oamk.fi>
2016-11-16 09:53:27 +0200

    Changed menu

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -9,7 +9,7 @@
 <ul>
 	<li><a href="<?php echo site_url('etusivu'); ?>">Etusivu</a></li>
 	<li><a href="<?php echo site_url('asiakas/lisaa'); ?>">Lisää asiakas</a></li>
-	<li><a href="<?php echo site_url('asiakas/nayta_muokattavat_asiakkaat'); ?>">Muokkaa asiakastietoja</a></li>
+	<li><a href="<?php echo site_url('asiakas/nayta_muokattavat_asiakkaat'); ?>">Muokkaa/poista asiakkaita</a></li>
 	<li><a href="<?php echo site_url('kortti/nayta_kortit'); ?>">Kulkukortit</a></li>
 	<li><a href="<?php echo site_url('kulkeminen/nayta_kulkemiset'); ?>">Kulkuhistoria</a></li>
 	<li><a href="<?php echo site_url('etusivu/logout'); ?>">Kirjaudu ulos</a></li>
