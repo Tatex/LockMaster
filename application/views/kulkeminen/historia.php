e1794d3ba330b42e03125213df161885bc9635c9
Antti Kyllönen <t4kyan01@students.oamk.fi>
2016-11-16 09:34:28 +0200

    Fix to previous commit & improvements to Kulkuhistoria

--- application/views/kulkeminen/historia.php
+++ application/views/kulkeminen/historia.php
@@ -1,18 +1,30 @@
 <FORM action="paivita_halytys" method="POST">
 <TABLE class="table table-striped table-bordered">
-<TR><TH>Päivä</TH><TH>Aika</TH><TH>Oven avaus</TH><TH>PIN syötetty</TH></TR>
+<TR><TH>Päivä</TH><TH>Aika</TH><TH>Kulkija</TH><TH>Oven avaus</TH><TH>PIN syötetty</TH></TR>
 <?php
 foreach ($kulkemiset as $rivi) {
 	echo '<tr>';
-	echo '<td>'.$rivi['pvm'].'</td>';
-	echo '<td>'.$rivi['aika'].'</td>';
-	echo '<td>'.$rivi['ovi_auki'].'</td>';
-	echo '<td>'.$rivi['ilta_kulku'].'</td>';
+
+	$paivays = DateTime::createFromFormat("Y-m-d", $rivi['pvm']);
+	$aika = DateTime::createFromFormat("H:i:s", $rivi['aika']);
+
+	echo '<td>'.$paivays->format("d.m.Y").'</td>';
+	echo '<td>'.$aika->format("H:i").'</td>';
+	echo '<td>'.$rivi['etunimi'].' '.$rivi['sukunimi'].'</td>';
+
+	if ($rivi['ovi_auki'])
+		echo '<td>Kyllä</td>';
+	else
+		echo '<td>Ei</td>';
+
+	if ($rivi['ilta_kulku'])
+		echo '<td>Kyllä</td>';
+	else
+		echo '<td>Ei</td>';
+	
 	echo '</tr>';
 
 }
 ?>
 </TABLE>
-	<input class="btn btn-primary" type="submit" name="btnTallenna" value="Tallenna" >
-	<a href="<?php echo site_url('asiakas/listaa');?>" class="btn btn-primary" role="button">Peruuta</a>
 </FORM>
